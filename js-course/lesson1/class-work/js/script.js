fba69cb9c9f733bcbbef3e7ffa2114de21014b0f
ArGus20 <artemhusar2007@gmail.com>
2022-07-22 21:24:50 +0300

    lesson1

---
+++ js-course/lesson1/class-work/js/script.js
@@ -0,0 +1,17 @@
+let firstName = 'Artem';
+
+console.log(firstName);
+
+firstName = 'Max';
+
+console.log(firstName);
+
+firstName = 29;
+
+console.log(firstName);
+
+const myBirthday = '15.09.1969';
+
+console.log(myBirthday);
+
+myBirthday = '11.10.1975';
---
+++ js-course/lesson1/home-work/js/script.js
@@ -0,0 +1,59 @@
+let x = 1080;
+
+console.log(x);
+console.log(typeof x);
+
+
+let str = 'Привіт світ!';
+
+console.log(str);
+console.log(typeof str);
+
+
+let str1 ='Артем';
+
+console.log(`З днем народження, ${str1}!`);
+console.log(typeof str1);
+
+
+let Artem;
+
+console.log(Artem);
+console.log(typeof Artem);
+
+
+let a = '10';
+let b = '2';
+
+console.log(a / b);
+
+
+let a1 = '12';
+let b1 = '5';
+
+console.log(a1 * b1);
+
+
+let str2 = 'Some words';
+
+console.log(Number(str2))
+
+
+let num = 25.07;
+
+console.log(`У мене день народження ${num}`);
+console.log(typeof num);
+
+
+let a2 = 123,
+    b2 = 321;
+
+console.log(a2 > b2);
+console.log(a2 < b2);
+
+
+let a3 = 1080,
+    b3 = 1000;
+
+console.log(a3 < b3);
+console.log(a3 != b3);
